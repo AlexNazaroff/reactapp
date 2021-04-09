036797d81dfccd6d143896abd3e0fdd2b401aa11
AlexNazaroff <ale.krotov@gmail.com>
2021-04-09 18:12:11 +0500

    4.1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Todolist from "./todo/todolist";
 
 function App() {
+
+    const todos = []
     return (<div className='wrapper'>
         <h1>React tutorial</h1>
         <Todolist />
